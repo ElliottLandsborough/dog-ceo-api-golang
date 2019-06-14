d4e9ae0f0d2e8af15eda8b15e1f93bdfcb14b763
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-14 16:39:38 +0100

    Output some json

--- getAllBreeds/main.go
+++ getAllBreeds/main.go
@@ -1,67 +1,81 @@
 package main
 
 import (
-	//"errors"
 	"fmt"
-	//"io/ioutil"
-	//"net/http"
 	"os"
+	"strings"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	//"github.com/aws/aws-sdk-go/aws"
-    //"github.com/aws/aws-sdk-go/aws/session"
-)
-
-var (
-	/*
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-	*/
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	/*
+	var responseCode int
+	var responseBody string
+	var bucket = os.Getenv("IMAGE_BUCKET_NAME");
+
 	sess, err := session.NewSession(&aws.Config{
-    	Region: aws.String("eu-west-1")},
+    	Region: aws.String(os.Getenv("BUCKET_REGION"))},
 	)
-	*/
+
+	svc := s3.New(sess)
+
+	input := &s3.ListObjectsV2Input{
+		Bucket:  aws.String(bucket),
+		Delimiter:  aws.String("/"),
+		Prefix:  aws.String(""),
+		MaxKeys: aws.Int64(1000000),
+	}
+
+	response, err := svc.ListObjectsV2(input)
+
+	// handle the error...
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+				responseCode = 404
+				responseBody = aerr.Error()
+			default:
+				fmt.Println(aerr.Error())
+				responseCode = 500
+				responseBody = aerr.Error()
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+			responseCode = 500
+			responseBody = err.Error()
+		}
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("%v", responseBody),
+			StatusCode: responseCode,
+		}, nil
+	}
+
+	var result []string
+	for _, c := range response.CommonPrefixes {
+		item := strings.TrimRight(*c.Prefix, "/")
+		//item = strings.TrimLeft(item, "page/")
+		result = append(result, item)
+	}
+
+	resultJson, _ := json.Marshal(result)
+
+	fmt.Println(resultJson)
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("%v", os.Getenv("BUCKET_NAME")),
+		Body:       string(resultJson),
 		StatusCode: 200,
 	}, nil
-	/*
-	resp, err := http.Get(DefaultHTTPGetAddress)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
-		StatusCode: 200,
-	}, nil
-	*/
 }
 
 func main() {
