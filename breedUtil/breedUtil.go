3f2f30fa0d1f09f2db791d6660223a71820b3b96
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-19 12:21:35 +0100

    Separate getAllBreeds, format output properly

--- breedUtil/breedUtil.go
+++ breedUtil/breedUtil.go
@@ -1,8 +1,94 @@
 package lib
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
 
-// This func must be Exported, Capitalized, and comment added.
-func Demo() {
-    fmt.Println("HI")
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// GetAllBreeds gets all breeds
+func GetAllBreeds() map[string][]string {
+	// @todo: deal with the errors in this function
+	bucket := os.Getenv("IMAGE_BUCKET_NAME")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("BUCKET_REGION"))},
+	)
+
+	svc := s3.New(sess)
+
+	input := &s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Delimiter: aws.String("/"),
+		Prefix:    aws.String(""),
+		MaxKeys:   aws.Int64(1000000),
+	}
+
+	response, err := svc.ListObjectsV2(input)
+
+	//var responseCode int
+	//var responseBody string
+
+	// handle the error...
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+				//responseCode = 404
+				//responseBody = aerr.Error()
+			default:
+				fmt.Println(aerr.Error())
+				//responseCode = 500
+				//responseBody = aerr.Error()
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+			//responseCode = 500
+			//responseBody = err.Error()
+		}
+		/*return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("%v", responseBody),
+			StatusCode: responseCode,
+		}, nil*/
+	}
+
+	// create map of string arrays
+	twoDimensionalArray := map[string][]string{}
+
+	// loop through aws result
+	for _, c := range response.CommonPrefixes {
+		breed := strings.TrimRight(*c.Prefix, "/")
+
+		// explode by -
+		exploded := strings.Split(breed, "-")
+
+		// master breed will always be at 0
+		master := exploded[0]
+
+		_, ok := twoDimensionalArray[master]
+
+		// master breed isn't in 2d array yet, add it
+		if !ok {
+			twoDimensionalArray[master] = []string{}
+		}
+
+		// sub breed exists
+		if len(exploded) > 1 {
+			// sub will always be 1
+			sub := exploded[1]
+
+			// append item to slice
+			twoDimensionalArray[master] = append(twoDimensionalArray[master], sub)
+		}
+	}
+
+	return twoDimensionalArray
 }
--- getAllBreeds/main.go
+++ getAllBreeds/main.go
@@ -1,81 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
 	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
-
-    breedUtil "../breedUtil"
+	breedUtil "../breedUtil"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var responseCode int
-	var responseBody string
-	var bucket = os.Getenv("IMAGE_BUCKET_NAME");
 
-	sess, err := session.NewSession(&aws.Config{
-    	Region: aws.String(os.Getenv("BUCKET_REGION"))},
-	)
+	result := breedUtil.GetAllBreeds()
 
-	svc := s3.New(sess)
-
-	input := &s3.ListObjectsV2Input{
-		Bucket:  aws.String(bucket),
-		Delimiter:  aws.String("/"),
-		Prefix:  aws.String(""),
-		MaxKeys: aws.Int64(1000000),
-	}
-
-	response, err := svc.ListObjectsV2(input)
-
-	// handle the error...
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case s3.ErrCodeNoSuchBucket:
-				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
-				responseCode = 404
-				responseBody = aerr.Error()
-			default:
-				fmt.Println(aerr.Error())
-				responseCode = 500
-				responseBody = aerr.Error()
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-			responseCode = 500
-			responseBody = err.Error()
-		}
-		return events.APIGatewayProxyResponse{
-			Body:       fmt.Sprintf("%v", responseBody),
-			StatusCode: responseCode,
-		}, nil
-	}
-
-	var result []string
-	for _, c := range response.CommonPrefixes {
-		item := strings.TrimRight(*c.Prefix, "/")
-		//item = strings.TrimLeft(item, "page/")
-		result = append(result, item)
-	}
-
-	resultJson, _ := json.Marshal(result)
-
-	breedUtil.Demo()
+	resultJSON, _ := json.Marshal(result)
 
 	return events.APIGatewayProxyResponse{
-		Body:       string(resultJson),
+		Body:       string(resultJSON),
 		StatusCode: 200,
 	}, nil
 }
