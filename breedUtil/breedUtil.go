9303733ce5c12cec2a052994a1a97039a4b9b171
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-21 16:05:22 +0100

    Add function to compare slices

--- breedUtil/breedUtil.go
+++ breedUtil/breedUtil.go
@@ -265,6 +265,20 @@ func getRandomBreedImageByBreedString(breed string) string {
 	return image
 }
 
+// stringSlicesAreEqual tells whether a and b contain the same elements.
+// A nil argument is equivalent to an empty slice.
+func stringSlicesAreEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func shuffleSlice(slice []string) []string {
 	// initialize global pseudo random generator
 	rand.Seed(time.Now().Unix())
