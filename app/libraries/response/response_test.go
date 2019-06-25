4eca982ee95fb3efc5d25bac86f0b6fd6d297867
Elliott Landsborough <elliott.landsborough@gmail.com>
2019-06-25 10:24:08 +0100

    Test more functions in response util

--- app/libraries/response/response_test.go
+++ app/libraries/response/response_test.go
@@ -23,3 +23,79 @@ func TestJsonResponse(t *testing.T) {
 		t.Errorf("Incorrect, got: %T, want: %T.", response, wanted)
 	}
 }
+
+func TestBreedResponseOneDimensional(t *testing.T) {
+	s := []string{"string1", "string2", "string3", "string4", "string5"}
+	result := BreedResponseOneDimensional(s)
+
+	if result.StatusCode != 200 {
+		t.Errorf("Incorrect, got: %d, want: %d.", result.StatusCode, 200)
+	}
+
+	expectedHeaders := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	if reflect.DeepEqual(expectedHeaders, result.Headers) != true {
+		t.Errorf("Incorrect, got: %s, want: %s.", result.Headers, expectedHeaders)
+	}
+
+	expectedJSON := `{"message":["string1","string2","string3","string4","string5"],"status":"success"}`
+
+	if expectedJSON != result.Body {
+		t.Errorf("Incorrect, got: %s, want: %s.", result.Body, expectedJSON)
+	}
+}
+
+func TestBreedResponseTwoDimensional(t *testing.T) {
+	s := map[string][]string{
+		"breed1": []string{
+			"subbreed1",
+			"subbreed2",
+		},
+		"breed2": []string{},
+	}
+
+	result := BreedResponseTwoDimensional(s)
+
+	if result.StatusCode != 200 {
+		t.Errorf("Incorrect, got: %d, want: %d.", result.StatusCode, 200)
+	}
+
+	expectedHeaders := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	if reflect.DeepEqual(expectedHeaders, result.Headers) != true {
+		t.Errorf("Incorrect, got: %s, want: %s.", result.Headers, expectedHeaders)
+	}
+
+	expectedJSON := `{"message":{"breed1":["subbreed1","subbreed2"],"breed2":[]},"status":"success"}`
+
+	if expectedJSON != result.Body {
+		t.Errorf("Incorrect, got: %s, want: %s.", result.Body, expectedJSON)
+	}
+}
+
+func TestImageResponseOneDimensional(t *testing.T) {
+	s := []string{"string1", "string2", "string3", "string4", "string5"}
+	result := ImageResponseOneDimensional(s)
+
+	if result.StatusCode != 200 {
+		t.Errorf("Incorrect, got: %d, want: %d.", result.StatusCode, 200)
+	}
+
+	expectedHeaders := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	if reflect.DeepEqual(expectedHeaders, result.Headers) != true {
+		t.Errorf("Incorrect, got: %s, want: %s.", result.Headers, expectedHeaders)
+	}
+
+	expectedJSON := `{"message":["string1","string2","string3","string4","string5"],"status":"success"}`
+
+	if expectedJSON != result.Body {
+		t.Errorf("Incorrect, got: %s, want: %s.", result.Body, expectedJSON)
+	}
+}
